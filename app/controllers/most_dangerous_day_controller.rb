27ada2ffd130d0ea43e9b68d0d930ebd4f2b01ee
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2018-04-11 09:50:13 -0600

    feature tests with most dangerous day

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,4 +1,8 @@
 class MostDangerousDayController < ApplicationController
   def index
+    @sd = params["start_date"].to_date
+    @start_date = @sd.strftime("%B %e, %Y")
+    @ed = params["end_date"].to_date
+    @end_date = @ed.strftime("%B %e, %Y")
   end
 end
---
+++ spec/features/user_can_see_most_dangerous_day_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+# Feature: A guest user visits the root page and enters a start date and end date.
+# The resulting page will display the most dangerous day in that range based on which day has the most `is_potentially_dangerous_asteroid` set to `true`.
+# Given the scenario below, it should be January 1, 2018 with 3 potentially dangerous asteroids.
+#
+
+describe "As a guest user" do
+  it "will show the most dangerous day" do
+    visit '/'
+
+    fill_in "start_date", with: "2018-01-01"
+    fill_in "end_date", with: "2018-01-07"
+
+    click_on "Determine Most Dangerous Day"
+
+    expect(current_path).to eq("/most_dangerous_day")
+    expect(page).to have_content("Most Dangerous Day")
+    expect(page).to have_content("January 1, 2018 - January 7, 2018")
+    expect(page).to have_content("January 1, 2018 has 3 potentially dangerous near earth objects")
+  end
+end
+# And I should see 3 asteroids in that list
+#
+# And I should see "Name: (2014 KT76)"
+# And I should see "NEO Reference ID: 3672906"
+#
+# And I should see "Name: (2001 LD)"
+# And I should see "NEO Reference ID: 3078262"
+#
+# And I should see "Name: (2017 YR1)"
+# And I should see "NEO Reference ID: 3794979"
